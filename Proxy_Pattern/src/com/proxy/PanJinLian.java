dc9ce1dd5136cfb9c1b30a5f38b7240233c38362
Inmovation <Inmovation@Inmovation-PC>
2015-10-13 10:13:53 +0800

    update

---
+++ Proxy_Pattern/src/com/proxy/PanJinLian.java
@@ -0,0 +1,23 @@
+package com.proxy;
+
+/**
+* @author cbf4Life cbf4life@126.com
+* I'm glad to share my knowledge with you all.
+* 定一个潘金莲是什么样的人
+*/
+
+public class PanJinLian implements KindWomen {
+
+	@Override
+	public void makeEyesWithMan() {
+		// TODO Auto-generated method stub
+		System.out.println("潘金莲在和男人做那个.....");
+	}
+
+	@Override
+	public void happyWithMan() {
+		// TODO Auto-generated method stub
+		System.out.println("潘金莲抛媚眼");
+	}
+
+}
