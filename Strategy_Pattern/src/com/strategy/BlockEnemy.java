33a66829561ff0fa00c3fed194e66723c8c718ea
Inmovation <Inmovation@Inmovation-PC>
2015-10-13 10:15:14 +0800

    update

---
+++ Strategy_Pattern/src/com/strategy/BlockEnemy.java
@@ -0,0 +1,17 @@
+package com.strategy;
+
+/**
+* @author cbf4Life cbf4life@126.com
+* I'm glad to share my knowledge with you all.
+* 孙夫人断后，挡住追兵
+*/
+
+public class BlockEnemy implements IStrategy {
+
+	@Override
+	public void operate() {
+		// TODO Auto-generated method stub
+		System.out.println("孙夫人断后，挡住追兵");
+	}
+
+}
